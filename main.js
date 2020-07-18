5ba0b6d465ac474fd7729dd59a5e15c2509b946b
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-07-19 09:25:26 +1000

    Initialize new projecct

---
+++ main.js
@@ -0,0 +1,30 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+const pAequorFactory = (num, strand)  => {
+  return {
+    specimenNum: num,
+    dna: strand
+  }
+}
+
+ console.log(pAequorFactory(2, mockUpStrand()))
+
+
+
+
+
+
+
