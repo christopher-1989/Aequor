93099b96547e0b77b53135df3cda613c5535b39a
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-07-19 09:45:11 +1000

    DNA compare method working

--- main.js
+++ main.js
@@ -16,12 +16,29 @@ const mockUpStrand = () => {
 const pAequorFactory = (num, strand)  => {
   return {
     specimenNum: num,
-    dna: strand
+    dna: strand,
+    mutate() {
+      this.dna = mockUpStrand()
+    },
+    compareDNA(pAequor) {
+      let total = 0
+      for (let i =0; i < pAequor.dna.length;i++){
+        if (pAequor.dna[i] === this.dna[i]) {
+          total ++
+        }
+      }
+      const percentage = Math.floor(total/15 * 100)
+      console.log(`specimen #1 and specimen #2 have ${percentage}% DNA in common`)
+    }
   }
 }
 
- console.log(pAequorFactory(2, mockUpStrand()))
-
+const Aequor = pAequorFactory(1, mockUpStrand())
+console.log(Aequor)
+Aequor.mutate()
+console.log(Aequor)
+const Aequor2 = pAequorFactory(2, mockUpStrand())
+Aequor2.compareDNA(Aequor)
 
 
 
